e1e24ecd7bd5aad56e2ac67dea92663f3ab479de
GuiFV <guilhermeviotti@gmail.com>
2023-07-01 17:00:21 +0100

    email settings implementation

--- life/settings.py
+++ life/settings.py
@@ -29,6 +29,18 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=Csv())
 
+# Email configuration
+
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+RECIPIENT_ADDRESS = config('RECIPIENT_ADDRESS')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+
 # Application definition
 
 INSTALLED_APPS = [
