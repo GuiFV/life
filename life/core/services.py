cfede7eccaa9c61b25ca74707e95d7220dcf82ed
GuiFV <guilhermeviotti@gmail.com>
2023-07-01 17:43:56 +0100

    new version of google agenda url slicer

--- life/core/services.py
+++ life/core/services.py
@@ -1,6 +1,21 @@
 from urllib.parse import urlparse, parse_qsl
 
+# V1
+# def extract_src_ctz(url: str) -> str:
+#     calendar_url = 'https://calendar.google.com/calendar'
+#     if calendar_url in url:
+#         url = url[url.index(calendar_url):]
+#         parsed_url = urlparse(url)
+#         query_params = parse_qsl(parsed_url.query)
+#         new_query = ''
+#         for key, value in query_params:
+#             if key in ['src', 'ctz']:
+#                 value = value.split()[0].replace("'", "").replace('"', '')
+#                 new_query += f'&{key}={value}'
+#         return new_query.lstrip('&')
+#     return ''
 
+# V2
 def extract_src_ctz(url: str) -> str:
     calendar_url = 'https://calendar.google.com/calendar'
     if calendar_url in url:
@@ -10,7 +25,7 @@ def extract_src_ctz(url: str) -> str:
         new_query = ''
         for key, value in query_params:
             if key in ['src', 'ctz']:
-                value = value.split()[0].replace("'", "").replace('"', '')
+                value = value.split(maxsplit=1)[0].split('%', 1)[0].replace("'", "").replace('"', '')
                 new_query += f'&{key}={value}'
         return new_query.lstrip('&')
     return ''
